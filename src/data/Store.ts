9f48aad44837e5944ca1f30775acac52261ec2ed
Tony <indrajit.suryakanta.9@gmail.com>
2024-01-25 10:43:21 +0530

    test

--- src/data/Store.ts
+++ src/data/Store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/userSlice';
+import offersSlice from './reducers/offersSlice';
+import transactionsSlice from './reducers/transactionsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    offers: offersSlice,
+    transactions: transactionsSlice,
   },
 });
 
